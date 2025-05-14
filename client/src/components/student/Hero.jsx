b2775b878e34861845a7fd007cbf154a1e93dfef
Saurabh-Sithkar <sithkar03@gmail.com>
2025-05-15 00:33:13 +0530

    Companies Section

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 const Hero = () => {
+
+
+
+  
   return (
     <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7
     md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
@@ -20,6 +25,7 @@ const Hero = () => {
         We bring together world-class instructors to help you achieve
         your professional goals
       </p>
+      <SearchBar/>
     </div>
   )
 }
