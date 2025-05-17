18473fc20135d20330fb6ca86a3ab5fff7944f63
Saurabh-Sithkar <sithkar03@gmail.com>
2025-05-17 14:58:41 +0530

    Educator Dashboard Left Section Completed

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../../context/AppContext';
+import { dummyDashboardData } from '../../assets/assets';
+import Loading from '../../components/student/Loading';
 
 const Dashboard = () => {
-  return (
-    <div>
-        <h1>Educator Dashboard</h1>
+
+  const {currency} = useContext(AppContext)
+  const [dashboardData, setDashboardData] = useState(null)
+
+  const fetchDashboarddData = async ()=>{
+    setDashboardData(dummyDashboardData)
+  }
+
+  useEffect(()=>{
+      fetchDashboarddData()
+  },[])
+
+  return dashboardData ? (
+    <div className='min-h-screen flex flex-col items-start justify-between gap-8
+    md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+      <div className='space-y-5'>
+        <div className='flex flex-wrap gap-5 items-center'>
+          
+        </div>
+      </div>
+        
     </div>
-  )
+  ) : <Loading/>
 }
 
 export default Dashboard
